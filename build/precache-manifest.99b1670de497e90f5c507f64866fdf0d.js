9da09c466b6c95c88ed712dac8a38ae8fc755d4e
zhutchings <zachhutchings2@gmail.com>
2022-07-19 21:17:16 -0600

    is this working

---
+++ build/precache-manifest.99b1670de497e90f5c507f64866fdf0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "09b3ad275988350334c2",
+    "url": "/static/css/main.f25f53a1.chunk.css"
+  },
+  {
+    "revision": "09b3ad275988350334c2",
+    "url": "/static/js/main.09b3ad27.chunk.js"
+  },
+  {
+    "revision": "f24a59d3e32d473fe88d",
+    "url": "/static/js/1.f24a59d3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f9abfa740dc1e6ad41a5e679dffe2718",
+    "url": "/index.html"
+  }
+];
